2a100eeeef917411ba94f5da394cdcf9b822a218
jkmalan <johnkmalan@gmail.com>
2019-12-16 16:49:13 -0500

    [ADD] Add requirements.txt for dependency management

--- wsgi.py
+++ wsgi.py
@@ -1,10 +1,9 @@
 #!/usr/bin/env python
 
 import sys
-import site
-
-site.addsitedir('/var/www/html/malandrakis/microcenter/private/lib/python3.7/site-packages')
 
 sys.path.insert(0, '/var/www/html/malandrakis/microcenter/public')
 
-from project.run import app as application
+from project.app import create_app
+
+application = create_app()
